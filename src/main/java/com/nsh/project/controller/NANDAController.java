65d61bab4277dd1320fa69a3602ffd418163c01d
Arley David Velasco <arley2180984@correo.uis.edu.co>
2021-08-15 08:56:37 -0500

    first commit

---
+++ src/main/java/com/nsh/project/controller/NANDAController.java
@@ -0,0 +1,40 @@
+package com.nsh.project.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/nanda")
+public class NANDAController {
+//get one
+    @GetMapping("/findById/{idNanda}")
+    public ResponseEntity<?> findById(
+            @PathVariable(name = "idNanda") Long aIdTipoCP) {
+        return ResponseEntity.ok(true);
+    }
+
+    //get all
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll() {
+        return ResponseEntity.ok(true);
+    }
+
+    //create one
+    @PostMapping("")
+    public ResponseEntity<?> crearNanda() {
+        return ResponseEntity.ok(true);
+    }
+
+    //update one
+    @PutMapping("")
+    public ResponseEntity<?> updateNanda(
+            @PathVariable(name = "idNanda") Long aIdTipoCP) {
+        return ResponseEntity.ok(true);
+    }
+
+    //delete one
+    @DeleteMapping("{idNanda}")
+    public ResponseEntity<?> deleteById(@PathVariable(name = "idNanda") Long aIdTipoCP) {
+        return ResponseEntity.ok(true);
+    }
+}
---
+++ src/test/java/com/nsh/project/NshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsh.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
